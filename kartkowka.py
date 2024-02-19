ad77eb5083d7ae46e89b55d529fcf858ea948b70
wiciuberger <145281618+wiciuberger@users.noreply.github.com>
2024-02-19 11:37:15 +0100

    Update kartkowka.py

--- kartkowka.py
+++ kartkowka.py
@@ -1,12 +1,17 @@
 class Uczen:
     liczba_uczniow = 0
+    @staticmethod
+    def srednia(lista):
+        srednia=sum(lista)/len(lista)
+        return srednia
+    
     def __init__(self, imie, nazwisko) -> None:
         self.imie = imie
         self.nazwisko = nazwisko
-        self.oceny_mat = [2,3,4,5,6]
-        self.srednia_ocen_mat = 0
-        self.oceny_ang = [5,6,6,6,6,6,6,6]
-        self.srednia_ocen_ang = 0
+        self.oceny_mat = [1, 2]
+        self.srednia_ocen_mat = self.srednia(self.oceny_mat)
+        self.oceny_ang = [5, 6]
+        self.srednia_ocen_ang = self.srednia(self.oceny_ang)
         Uczen.liczba_uczniow += 1
 
     def inf(self):
@@ -18,7 +23,7 @@ class Uczen:
         print(f"średnia ang {self.srednia_ocen_ang}")
 
     def policz(self, oceny):
-        if len(oceny) >0:
+        if len(oceny) > 0:
             return sum(oceny)/len(oceny)
         else:
             return 0
@@ -36,13 +41,48 @@ class Uczen:
 
 print("==="*10)
 print("==="*10)
-uczen1 = Uczen("Adrianek", "Poniatowski")
+uczen1 = Uczen("Olek", "Olkowski")
 uczen1.inf()
 print("==="*10)
 print("==="*10)
 print("==="*10)
 uczen1.edytuj_imie("Alex")
-uczen1.usun_szóstki([2,3,4,5], [5])
+print("Nie ma takiej oceny na liscie")
 uczen1.liczba_stworzonych_uczniow()
 uczen1.inf()
-print("Liczba stworzonych uczniów:", Uczen.liczba_stworzonych_uczniow())
+
+
+
+class Matematyka:
+
+    @staticmethod
+    def ile_liczb_parzystych():
+        count = 0
+        for liczba in range(100):
+            if liczba % 2 == 0:
+                count += 1
+        return count
+
+    @staticmethod
+    def czy_pierwsza(liczba: int):
+        if liczba < 2:
+            return False
+        for i in range(2, int(liczba ** 0.5) + 1):
+            if liczba % i == 0:
+                return False
+        return True
+
+    @staticmethod
+    def ile_liczb_pierwszych():
+        count = 0
+        for liczba in range(100):
+            if Matematyka.czy_pierwsza(liczba):
+                count += 1
+        return count
+
+
+from random import randint
+liczby = [i for i in range(100)]
+
+print("Ilość liczb parzystych:", Matematyka.ile_liczb_parzystych())
+print("Ilość liczb pierwszych:", Matematyka.ile_liczb_pierwszych())
